3eb3f3f09aeaf3040f570296a0f9176ff5ff773e
Nelonn <thenelonn@gmail.com>
2023-03-02 01:16:27 +0300

    Added blank module

---
+++ propack-core/src/main/java/me/nelonn/propack/module/JavaModule.java
@@ -0,0 +1,21 @@
+package me.nelonn.propack.module;
+
+public class JavaModule implements Module { // TODO: module loading system
+    @Override
+    public final void enable() {
+
+    }
+
+    protected void onEnable() {
+
+    }
+
+    @Override
+    public final void disable() {
+
+    }
+
+    protected void onDisable() {
+
+    }
+}
