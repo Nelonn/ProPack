e834143e5c5e0c0465ef57da38272ed8a8940b31
Nelonn <42481486+Nelonn@users.noreply.github.com>
2023-03-25 17:02:31 +0300

    0.0.7 (#15)
    * 1.19.4 Support

---
+++ propack-bukkit/adapters/adapter-1.19.4/build.gradle.kts
@@ -0,0 +1,27 @@
+import org.cadixdev.gradle.licenser.LicenseExtension
+
+plugins {
+    `java-library`
+    id("org.cadixdev.licenser")
+    id("io.papermc.paperweight.userdev")
+}
+
+java.toolchain.languageVersion.set(JavaLanguageVersion.of(17))
+
+dependencies {
+    // https://papermc.io/repo/service/rest/repository/browse/maven-public/io/papermc/paper/dev-bundle/
+    paperDevBundle("1.19.4-R0.1-20230315.180636-1")
+    "compileOnly"(files("../../../libs/flint-path-0.0.1.jar"))
+    "compileOnly"(project(":propack-api"))
+    "compileOnly"(project(":propack-core"))
+    "compileOnly"(project(":propack-bukkit"))
+}
+
+tasks.named("assemble") {
+    dependsOn("reobfJar")
+}
+
+configure<LicenseExtension> {
+    header(rootProject.file("HEADER.txt"))
+    include("**/*.java")
+}
