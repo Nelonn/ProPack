69098e87c4c156422e9775c5b597fafb8742052a
Nelonn <thenelonn@gmail.com>
2023-03-25 17:01:20 +0300

    1.19.4 Support

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -15,6 +15,6 @@ include("propack-cli")
 
 include("propack-bukkit")
 
-listOf("1.17.1", "1.18.2", "1.19", "1.19.3").forEach {
+listOf("1.17.1", "1.18.2", "1.19", "1.19.3", "1.19.4").forEach {
     include("propack-bukkit:adapters:adapter-$it")
 }
