4342a672ea3fef7b898a2245babd456cc8bb6f67
Nelonn <two.nelonn@gmail.com>
2023-09-29 22:47:12 +0300

    Updated paper versions

--- propack-bukkit/adapters/adapter-1.19.3/build.gradle.kts
+++ propack-bukkit/adapters/adapter-1.19.3/build.gradle.kts
@@ -10,7 +10,7 @@ java.toolchain.languageVersion.set(JavaLanguageVersion.of(17))
 
 dependencies {
     // https://papermc.io/repo/service/rest/repository/browse/maven-public/io/papermc/paper/dev-bundle/
-    paperweight.paperDevBundle("1.19.3-R0.1-20221226.180038-55")
+    paperweight.paperDevBundle("1.19.3-R0.1-20230312.180621-141")
     "compileOnly"(files("../../../libs/lib-flint-path-0.0.1.jar"))
     "compileOnly"(project(":propack-api"))
     "compileOnly"(project(":propack-core"))
