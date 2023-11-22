e6916c35eee6588eb916e82f19ba004f0c8ccccd
Nelonn <two.nelonn@gmail.com>
2023-11-22 12:33:12 +0300

    Bump 1.20.2 paper version

--- propack-bukkit/adapters/adapter-1.20.2/build.gradle.kts
+++ propack-bukkit/adapters/adapter-1.20.2/build.gradle.kts
@@ -7,7 +7,7 @@ java.toolchain.languageVersion.set(JavaLanguageVersion.of(17))
 
 dependencies {
     // https://papermc.io/repo/service/rest/repository/browse/maven-public/io/papermc/paper/dev-bundle/
-    paperweight.paperDevBundle("1.20.2-R0.1-20231014.214817-41")
+    paperweight.paperDevBundle("1.20.2-R0.1-20231122.053953-101")
     "compileOnly"(files("../../../libs/lib-flint-path-0.0.1.jar"))
     "compileOnly"(project(":propack-api"))
     "compileOnly"(project(":propack-core"))
