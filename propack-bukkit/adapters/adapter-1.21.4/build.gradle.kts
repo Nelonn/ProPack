a47505611b825b3915949f8c9e23d07340d64446
Nelonn <42481486+Nelonn@users.noreply.github.com>
2025-05-12 14:07:22 +0300

    Changed paperweight bundle version

--- propack-bukkit/adapters/adapter-1.21.4/build.gradle.kts
+++ propack-bukkit/adapters/adapter-1.21.4/build.gradle.kts
@@ -9,7 +9,7 @@ paperweight.reobfArtifactConfiguration = io.papermc.paperweight.userdev.ReobfArt
 
 dependencies {
     // https://repo.papermc.io/service/rest/repository/browse/maven-public/io/papermc/paper/dev-bundle/
-    paperweight.paperDevBundle("1.21.4-R0.1-20250309.181911-203")
+    paperweight.paperDevBundle("1.21.4-R0.1-20250511.205801-225")
     compileOnly(files("../../../libs/flint-path-0.0.1.jar"))
     compileOnly(project(":propack-api"))
     compileOnly(project(":propack-core"))
